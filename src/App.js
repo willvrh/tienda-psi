8d78542b4f1de4d353aeac2b532ff83b5c59a831
Enrique W. Vrhovski <evrhovski@gmail.com>
2021-08-01 12:44:12 -0300

    Se agrego componente ItemCount, falta agregar callback onAdd

--- src/App.js
+++ src/App.js
@@ -6,11 +6,20 @@ import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
 import ItemCount from './components/ItemCount';
 function App() {
+
+  const onAdd = () => {
+    console.log("ONADD")
+  };
+
   return (
       <Container disableGutters={true} maxWidth='xl'>
         <NavBar/>
         <ItemListContainer greetings="Bienvenido a la tienda!">
-        <ItemCount/>
+        <ItemCount 
+          stock='5'
+          initial='6'
+          onAdd={}
+        />
         </ItemListContainer>
 
       </Container>
--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -8,25 +8,67 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-  },
-});
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-export default function ItemCount({stock, initial, onAdd}) {
+import RemoveIcon from '@material-ui/icons/Remove';
+import IconButton from '@material-ui/core/IconButton';
+import AddIcon from '@material-ui/icons/Add';
+import { PanToolSharp } from '@material-ui/icons';
+
+const useStyles = makeStyles ((theme) => ({
+    root: {
+     maxWidth: 345,
+     },
+    buttonsDiv: {
+      flexGrow: 1,
+    },
+    paper: {
+        //padding: theme.spacing(2),
+        padding: '2px',
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        minHeight: '30px',
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+
+  
+}));
+
+export default function ItemCount(props) {
 
     const classes = useStyles();
+
+    const [stock, setStock] = useState(Number(props.stock));
+    const getValidInitial = (value) => {
+        let validInitial = 0;
+        return value < stock ? validInitial = value : validInitial = stock;
+    }
+
+    const [initial, setInitial] = useState(getValidInitial(Number(props.initial)));
+
+    const [onAdd, setOnAdd] = useState(props.onAdd);
+        
+
+    const addOne = () => {
+        if (initial < stock) {setInitial(initial+1)};
+    }
+
+    const substractOne = () => {
+        if (initial > 1) {setInitial(initial-1)};
+    } 
 
     return (
         <Card className={classes.root}>
         <CardActionArea>
             <CardMedia
             component="img"
-            alt="Contemplative Reptile"
+            alt="Imagen del producto"
             height="140"
             image={require('../assets/img/not_available.jpg').default}
-            title="Contemplative Reptile"
+            title="Imagen del producto"
             />
             <CardContent>
             <Typography gutterBottom variant="h5" component="h2">
@@ -38,13 +80,37 @@ export default function ItemCount({stock, initial, onAdd}) {
             </CardContent>
         </CardActionArea>
         <CardActions>
-            <Button size="small" color="primary">
-            Share
-            </Button>
-            <Button size="small" color="primary">
-            Learn More
+            <Grid container spacing={3}>
+                <Grid item xs>
+                    <Paper className={classes.paper}>
+                        <IconButton onClick={substractOne} size="small" aria-label="restar 1" color="inherit">
+                            <RemoveIcon />
+                        </IconButton>                        
+                    </Paper>
+                </Grid>
+                <Grid item xs>
+                    <Paper className={classes.paper}>
+                        <Typography variant="button" >
+                            {initial}
+                        </Typography>
+                    </Paper>
+                </Grid>
+                <Grid item xs>
+                    <Paper className={classes.paper}>
+                        <IconButton onClick={addOne} size="small" aria-label="agregar 1" color="inherit">
+                            <AddIcon />
+                        </IconButton>  
+                    </Paper>
+                </Grid>
+            </Grid>
+        </CardActions>
+
+        <CardActions>
+            <Button fullWidth={true} size="small" color="primary" onClick={props.onAdd}>
+            Agregar al carrito
             </Button>
         </CardActions>
+
         </Card>
     );
   }
